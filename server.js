1537e5603b3e303259fc57b2b350b3aac3ef1bbe
Milan Sułek <sulekmilan00@gmail.com>
2024-10-28 11:08:49 +0100

    server1

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 const http = require('http'); //import modulu http
 
 const server = http.createServer((req, res) =>{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Serwer działa!'); 
+    if(req.url === "/"){
+        res.write("Witaj na stronie głównej");
+        res.end();
+    }
+    else if(req.url === "/kontakt"){
+        res.write("Witaj na stronie kontaktowej");
+        res.end();
+    }
+    else{
+        res.write("404 - PAGE NOT FOUND");
+        res.end();
+    }
 })
 
 server.listen(3000, "127.0.0.1", ()=>{
-    console.log('Serwer dziala na porcie 3000 i adresie http://127.0.0.1:3000/);
+    console.log('Serwer dziala na porcie 3000 i adresie http://127.0.0.1:3000/');
 })
